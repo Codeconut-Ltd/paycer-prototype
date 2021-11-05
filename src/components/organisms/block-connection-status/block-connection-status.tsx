0c14347e0cc5be19fe427713327a8f1a401e41ed
Christian Oellers <2101569+ChristianOellers@users.noreply.github.com>
2021-11-05 18:06:37 +0100

    Proper display, still broken due to other issues

--- src/components/organisms/block-connection-status/block-connection-status.tsx
+++ src/components/organisms/block-connection-status/block-connection-status.tsx
@@ -8,20 +8,6 @@ import useNetwork from '@hooks/use-network'
 import useWallet from '@hooks/use-wallet'
 import { getExplorerBlockUrl } from '@providers/explorers'
 
-/**
- * z-index is intentionally below any of Bootstrap's components.
- *
- * @see https://getbootstrap.com/docs/5.0/layout/z-index
- */
-const StyledBlockConnectionStatus = styled.div`
-    z-index: 990;
-    background: rgba(0, 0, 0, 0.85);
-    border-radius: 0.25rem;
-    right: 1rem;
-    bottom: 1rem;
-    padding: 0.25rem 0.5rem;
-`
-
 export default function BlockConnectionStatus() {
   const network = useNetwork()
   const blockNumber: number | undefined = useBlockNumber()
@@ -42,7 +28,7 @@ export default function BlockConnectionStatus() {
   }, [blockNumber, network.chainId])
 
   return (
-    <StyledBlockConnectionStatus className="position-fixed">
+    <div className="portal-test">
       {href ? (
         <IndicatorItem state="success" title={t`View block details`}>
           <LinkExternal href={href}>
@@ -54,6 +40,6 @@ export default function BlockConnectionStatus() {
             {t`Disconnected`}
         </IndicatorItem>
       )}
-    </StyledBlockConnectionStatus>
+    </div>
   )
 }
